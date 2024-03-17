d8bb6976695ff1428812128870f88c7e10c4a8d9
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-17 17:10:44 -0300

    Método 04: .sort()

---
+++ Método4.js
@@ -0,0 +1,25 @@
+//Método 04: .sort().
+//O método.sort() ordena os elementos presentos dentro da array.
+
+//Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
+
+//Defino uma array com os números 5, 4, 3, 2 e 1 em ordem decrescente.
+const numeros = [5, 4, 3, 2, 1];
+//Exibo numeros.
+console.log(numeros);
+//Defino numeros2 como a ordem crescente da array numeros utilizando .sort().
+const numeros2 = numeros.sort();
+//Exibo numeros2.
+console.log(numeros2);
+
+//Exibo um espaço em branco para separar os dois exemplos.
+console.log("");
+
+//Defino uma array com os valores 50, 40, 30, 20 e 10.
+const numeros3 = [50, 40, 30, 20, 10];
+//Exibo a array numeros3
+console.log(numeros3);
+//Defino a a array numeros4 como a forma ordenada da array numeros3 com numeros.sort().
+const numeros4 = numeros3.sort();
+//Exibo numeros4
+console.log(numeros4);
