dabedf1dc2e05fc6b31e7f2c56fe423a2e40b450
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-17 17:17:34 -0300

    Método 07: Array.from()

---
+++ Método7.js
@@ -0,0 +1,19 @@
+//Método 07: Array.from().
+//O método Array.from() cria uma nova array à partir de um objeto semelhante a uma array ou iterável.
+
+//Defino uma varíavel com a string 'Thiago'.
+let objeto = 'Thiago';
+//Defino uma variável chamada exibidor com o método Array.from(objeto), de forma que eu crie uma array.
+let exibidor = Array.from(objeto);
+//Exibe a array.
+console.log(exibidor);
+
+//Exibo um espaço em branco para separar os exemplos.
+console.log("");
+
+//Defino uma varíavel com a string 'Vitor'
+let objeto2 = 'Vitor';
+//Defino uma variável chamada exibidor2 com o método Array.from(objeto), de forma que eu crie uma array.
+let exibidor2 = Array.from(objeto2);
+//Exibo a array
+console.log(exibidor2);
