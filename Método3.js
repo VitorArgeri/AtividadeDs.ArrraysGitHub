71f093346bc6faf74c0ed2d5cc0dedc7b59a5f01
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-17 17:07:36 -0300

    Método 03 .reverse()

---
+++ Método3.js
@@ -0,0 +1,25 @@
+//Método 03: .reverse()
+//O método .reverse() faz o que o próprio nome fala, ele inverte a array.
+
+//Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
+
+//Defino uma array com as strings 1, 2, 3, 4 e 5.
+const arrayA = [1, 2, 3, 4, 5];
+//Exibo a arrayA.
+console.log(arrayA);
+//Defino uma array como o reverso da arrayA com o .reverse().
+const arrayB = arrayA.reverse();
+//Exibo a arrayB.
+console.log(arrayB);
+
+//Exibo um espaço em branco para separar os exemplos.
+console.log("")
+
+//Defino uma array com as strings Abelha, Baleia, Crocodilo e Dinossauro.
+const arrayC = ["Abelha", "Baleia", "Crocodilo", "Dinossauro"];
+//Exibo a arrayC
+console.log(arrayC);
+//Defino uma array como o reverso da arrayC com o .reverse().
+const arrayD = arrayC.reverse();
+//Exibo a arrayD.
+console.log(arrayD);
