231988c6e2ef8bf10029a022e1400abbd6d7184a
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-17 17:05:41 -0300

    Método 02: .join()

---
+++ Método2.js
@@ -0,0 +1,25 @@
+//Método 02: .join()
+//O método join() junta todos os elementos de um array em uma string e retorna esta string.
+
+//Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
+
+//Defino uma array com os números 10, 20 e 30.
+const arrayA = [10, 20, 30];
+//Uso do método .join() para separar os elementos da arrayA com o parâmetro *.
+const arrayB = arrayA.join("*");
+//Exibo a arrayA.
+console.log(arrayA);
+//Exibo a arrayB.
+console.log(arrayB);
+
+//Exibo um espaço em branco para separar os dois exemplos.
+console.log("")
+
+//Defino uma array com as strings 'Aves', 'Mamíferos' e 'Répteis.
+const arrayC = ['Aves', 'Mamíferos', 'Répteis'];
+//Uso do método .join() para separar os elementos da arrayC com o parâmetro _.
+const arrayD = arrayC.join("_");
+//Exibo a arrayC.
+console.log(arrayC);
+//Exibo a arrayD.
+console.log(arrayD);
